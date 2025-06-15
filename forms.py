59187b6ecad9e431629d13e761391ebc3cce5b01
Syed Farmaan <syedfarmaan15@gmail.com>
2025-06-15 14:52:08 +0530

    Added a delete form

--- forms.py
+++ forms.py
@@ -1,9 +1,17 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,Length
 
 class ContactForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     message = TextAreaField("Message", validators=[DataRequired()])
     submit = SubmitField("Send")
+
+class ProjectForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(min=3, max=100)])
+    description = TextAreaField("Description", validators=[DataRequired(), Length(min=10)])
+    submit = SubmitField("Add Project")
+
+class DeleteForm(FlaskForm):
+    submit = SubmitField('Delete')
